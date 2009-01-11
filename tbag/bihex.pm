a0c9578d889180555e26f6537b58b8513d115019
Dale R. Anderson <lime.dale@gmail.com>
2009-01-11 02:05:39 -0800

    added bih command

---
+++ tbag/bihex.pm
@@ -0,0 +1,27 @@
+
+#######
+# a perl shell tbag leaf to...
+#
+#
+
+use pms::tmsg;
+use pms::argverify;
+
+tmsg "bihex init", -2;
+
+#...
+
+sub bihex {
+
+	return unless argverify(\@_, 0, "your sub error message if bad args are passed");
+
+
+
+}
+
+
+
+
+
+#return true to please perl
+;1;
